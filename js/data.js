b45573ee652a1e0aa7a985b193f1340547047fef
zhangqifei <1353250703@qq.com>
2018-03-25 19:48:21 +0800

    1

--- js/data.js
+++ js/data.js
@@ -19,18 +19,22 @@ var FrontGuide = {
             "pageC2":[
                 {'Name':'阿里源','Title':'阿里云开源软件镜像','Href':'https://opsx.alibaba.com/mirror'},
                 {'Name':'科大','Title':'中国科学技术大学开源软件镜像','Href':'http://centos.ustc.edu.cn/'},
-				{'Name':'网易','Title':'欢迎访问网易开源镜像站','Href':'http://mirrors.163.com/'}，
+				{'Name':'网易','Title':'欢迎访问网易开源镜像站','Href':'http://mirrors.163.com/'},
 				{'Name':'清华大学','Title':'清华大学开源软件镜像站','Href':'https://mirrors.tuna.tsinghua.edu.cn/'}
             ]
         }]},{
         "pageT1":"数据库",
         "pageC1":[{
-            "pageT2":"我的链接",
+            "pageT2":"数据库",
             "pageC2":[
-                {'Name':'我的博客','Title':'爱折腾，爱运动，更爱游离于错综复杂的编码与逻辑中，无法自拔。','Href':'http://zhangqifei.top/'}
+                {'Name':'MySQL','Title':'最流行的关系型数据库管理系统','Href':'https://www.mysql.com/downloads/'},
+                {'Name':'MariaDB ','Title':'也是最流行的关系型数据库管理系统 ','Href':'https://downloads.mariadb.org/'},
+                {'Name':'MongoDB ','Title':'一个基于分布式文件存储的数据库 ','Href':'https://www.mongodb.com/'},
+                {'Name':'Redis ','Title':'一个高性能的key-value数据库','Href':'https://redis.io/'},
+                {'Name':'SQLite ','Title':'SQLite 是在世界上最广泛部署的 SQL 数据库引擎','Href':'http://www.sqlite.org/index.html/'},
+                {'Name':'Memcached ','Title':'一个自由开源的，高性能，分布式内存对象缓存系统。','Href':'http://memcached.org/'}
             ]
-        }]},
-		{
+        }]},{
         "pageT1":"我的链接",
         "pageC1":[{
             "pageT2":"我的链接",
