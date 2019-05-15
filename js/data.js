a0c43ad5d97087bdfbb68a4ec379c07bdc22f05e
zhangqifei <1353250703@qq.com>
2019-05-15 17:55:19 +0800

    v1.0

--- js/data.js
+++ js/data.js
@@ -12,46 +12,111 @@
     {'Name':'','Title':'','Href':'',"New":"1"}, 
 */
 var FrontGuide = {
-    "main":[{
-			"pageT1":"圈子",
-			"pageC1":[{
-				"pageT2":"系统",
-				"pageC2":[
-					{'Name':'Centos','Title':'Centos官网','Href':'https://www.centos.org/'},
-					{'Name':'ubuntu','Title':'ubuntu官网','Href':'https://www.ubuntu.com/index_kylin'},
-					{'Name':'ubuntu','Title':'ubuntu官网','Href':'https://www.ubuntu.com/index_kylin'}
-				]
-		},{
-				"pageT2":"yum源",
-				"pageC2":[
-					{'Name':'阿里源','Title':'阿里云开源软件镜像','Href':'https://opsx.alibaba.com/mirror',"New":"1"},
-					{'Name':'科大','Title':'中国科学技术大学开源软件镜像','Href':'http://centos.ustc.edu.cn/'},
-					{'Name':'网易','Title':'欢迎访问网易开源镜像站','Href':'http://mirrors.163.com/'},
-					{'Name':'清华大学','Title':'清华大学开源软件镜像站','Href':'https://mirrors.tuna.tsinghua.edu.cn/'},
-					{'Name':'搜狐','Title':'搜狐开源软件镜像站','Href':'http://mirrors.sohu.com/'},
-					{'Name':'上海交大','Title':'上海交大开源软件镜像站','Href':'http://ftp.sjtu.edu.cn/centos/'},
-					{'Name':'CentOS官方标准源','Title':'CentOS官方开源软件镜像站','Href':'http://mirrors.ustc.edu.cn/centos/'},
-					{'Name':'北京首都在线科技','Title':'北京首都在线开源软件镜像站','Href':'http://mirrors.yun-idc.com/'}
-				]
-        }]},{
-        "pageT1":"数据库",
-        "pageC1":[{
-            "pageT2":"数据库",
-            "pageC2":[
-                {'Name':'MySQL','Title':'最流行的关系型数据库管理系统','Href':'https://www.mysql.com/downloads/'},
-                {'Name':'MariaDB ','Title':'也是最流行的关系型数据库管理系统 ','Href':'https://downloads.mariadb.org/'},
-                {'Name':'MongoDB ','Title':'一个基于分布式文件存储的数据库 ','Href':'https://www.mongodb.com/'},
-                {'Name':'Redis ','Title':'一个高性能的key-value数据库','Href':'https://redis.io/'},
-                {'Name':'SQLite ','Title':'SQLite 是在世界上最广泛部署的 SQL 数据库引擎','Href':'http://www.sqlite.org/index.html/'},
-                {'Name':'Memcached ','Title':'一个自由开源的，高性能，分布式内存对象缓存系统。','Href':'http://memcached.org/'}
+    "main":[
+        {
+            "pageT1": "圈子", 
+            "pageC1": [
+                {
+                    "pageT2": "官网", 
+                    "pageC2": [
+                        {"Name": "Centos", "Title": "CentOS 是Linux发行版之一", "Href": "https://www.centos.org/"}, 
+                        {"Name": "Ubuntu", "Title": "以桌面应用为主的开源GNU/Linux操作系统","Href": "https://www.ubuntu.com/index_kylin"}, 
+                        {"Name": "Docker","Title": "Docker 是一个开源的应用容器引擎","Href":"https://www.docker.com/"},
+                        {"Name": "kubernetes","Title": "K8S是Google开源的一个容器编排引擎","Href":"https://kubernetes.io/"},
+                        {"Name": "openstack","Title": "OpenStack是一个开源的云计算管理平台","Href":"https://www.openstack.org/"},
+                    ]
+                }, 
+                {
+                    "pageT2": "镜像源", 
+                    "pageC2": [
+                        {"Name": "阿里源","Title": "阿里巴巴镜像软件系统开源","Href":"https://opsx.alibaba.com/mirror","New": "1"}, 
+                        {"Name": "科大","Title": "中国科学技术大学开源软件镜像","Href": "http://centos.ustc.edu.cn/"}, 
+                        {"Name": "网易","Title": "欢迎访问网易开源镜像站", "Href": "http://mirrors.163.com/"}, 
+                        {"Name": "清华大学","Title": "清华大学开源软件镜像站","Href": "https://mirrors.tuna.tsinghua.edu.cn/"}, 
+                        {"Name": "搜狐","Title": "搜狐开源软件镜像站","Href": "http://mirrors.sohu.com/"}, 
+                        {"Name": "上海交大","Title": "上海交大开源软件镜像站","Href": "http://ftp.sjtu.edu.cn/centos/"}, 
+                        {"Name": "CentOS官方标准源","Title": "CentOS官方开源软件镜像站","Href": "http://mirrors.ustc.edu.cn/centos/"}, 
+                        {"Name": "北京首都在线科技", "Title": "北京首都在线开源软件镜像站","Href": "http://mirrors.yun-idc.com/"},
+                        {"Name": "北京理工大学","Title": "北京理工大学开源软件镜像服务","Href":"http://mirror.bit.edu.cn/web/"},
+                        {"Name": "RPM包下载","Title": "RPM包下载","Href":"http://rpmfind.net/linux/rpm2html/search.php?query=ntfs-3g"},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""}
+                    ]
+                }, 
+                {
+                    "pageT2": "公有云", 
+                    "pageC2": [
+                        {"Name": "阿里云","Title": "阿里云 - 上云就上阿里云","Href":"https://www.aliyun.com/"},
+                        {"Name": "腾讯云","Title": "腾讯云 - 产业智变 云启未来","Href":"https://cloud.tencent.com/"},
+                        {"Name": "亚马逊云-cn","Title": "AWS云服务-先进的可扩展云计算平台","Href":"https://www.amazonaws.cn/"},
+                        {"Name": "亚马逊云-com","Title": "AWS 云服务-专业的大数据和云计算服务以及云解决方案提供商","Href":"https://aws.amazon.com"},
+                        {"Name": "微软云","Title": "Microsoft Azure 云计算平台与服务","Href":"azure.microsoft.com"}
+                    ]
+                }, 
+                {
+                    "pageT2": "ISO镜像下载", 
+                    "pageC2": [
+                        {"Name": "Windows","Title": "MSDN, 我告诉你","Href":"https://msdn.itellyou.cn/"},
+                        {"Name": "CentOS","Title": "CentOS历史版本下载","Href":"http://vault.centos.org/"},
+                        {"Name": "CentOS镜像列表","Title": "镜像列表","Href":"https://www.centos.org/download/mirrors/"},
+                        {"Name": "","Title": "","Href":""}
+                    ]
+                },
+                {
+                    "pageT2": "软件", 
+                    "pageC2": [
+                        {"Name": "Tomcat","Title": "Web 应用服务器","Href":"https://tomcat.apache.org/"},
+                        {"Name": "Nginx","Title": "HTTP服务器和反向代理服务器","Href":"http://nginx.org/en/"},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""},
+                        {"Name": "","Title": "","Href":""}
+                    ]
+                }
             ]
-        }]},{
-        "pageT1":"我的链接",
-        "pageC1":[{
-            "pageT2":"我的链接",
-            "pageC2":[
-                {'Name':'我的博客','Title':'爱折腾，爱运动，更爱游离于错综复杂的编码与逻辑中，无法自拔。','Href':'http://zhangqifei.top/'}
+        }, 
+        {
+            "pageT1": "数据库", 
+            "pageC1": [
+                {
+                    "pageT2": "数据库", 
+                    "pageC2": [
+                        {"Name": "MySQL","Title": "最流行的关系型数据库管理系统","Href": "https://www.mysql.com/downloads/"}, 
+                        {"Name": "MariaDB ","Title": "也是最流行的关系型数据库管理系统","Href": "https://downloads.mariadb.org/"}, 
+                        {"Name": "MongoDB ","Title": "一个基于分布式文件存储的数据库 ","Href": "https://www.mongodb.com/"}, 
+                        { "Name": "Redis","Title": "一个高性能的key-value数据库","Href": "https://redis.io/"}, 
+                        {"Name": "SQLite","Title": "SQLite 是在世界上最广泛部署的 SQL 数据库引擎","Href": "http://www.sqlite.org/index.html/"}, 
+                        {"Name": "Memcached", "Title": "一个自由开源的，高性能，分布式内存对象缓存系统", "Href": "http://memcached.org/"}
+                    ]
+                },
+                {
+                    "pageT2": "SSL", 
+                    "pageC2": [
+                        {"Name": "Certbot","Title": "Certbot 免费https","Href": "https://certbot.eff.org/"},
+                        {"Name": "证书测试","Title": "SSL服务器测试","Href": "https://www.ssllabs.com/ssltest/index.html"},
+                        {"Name": "SSL 工具","Title": "SSL服务器证书安装检查器","Href": "https://www.myssl.cn/tools/check-server-cert.html"},
+                        {"Name": "chinassl","Title": "SSL证书格式转换工具","Href": "https://csr.chinassl.net/convert-ssl.html"}
+
+                    ]
+                },
             ]
-        }]}
-	]
+        }, 
+        {
+            "pageT1": "我的链接", 
+            "pageC1": [
+                {
+                    "pageT2": "我的链接", 
+                    "pageC2": [
+                        {"Name": "我的博客","Title": "爱折腾，爱运动，更爱游离于错综复杂的编码与逻辑中，无法自拔","Href": "http://zhangqifei.top/"}
+                    ]
+                }
+            ]
+        }
+    ]
 }
